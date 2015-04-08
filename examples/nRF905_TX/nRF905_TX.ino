3543c039f1aa56fa0ca95867e43c7e8389dccb5e
Achuthan Paramanathan <ap@es.aau.dk>
2015-04-08 19:24:54 +0200

    no message

--- examples/nRF905_TX/nRF905_TX.ino
+++ examples/nRF905_TX/nRF905_TX.ino
@@ -1,64 +1,70 @@
-/*  ============================================================================
-    Copyright (C) 2015 Achuthan Paramanathan.
-    ============================================================================
-    This program is free software; you can redistribute it and/or
-    modify it under the terms of the GNU General Public License
-    version 2 as published by the Free Software Foundation.
-    ============================================================================
-    Revision Information:
-        File name: nRF905_TX.ino
-        Version:   v0.0
-        Date:      05-04-2015
-    ==========================================================================*/
+#include <NRF905.h>
+#include <SPI.h>
 
-/*============================================================================*/
-/*                           INCLUDE STATEMENTS                               */
-/*============================================================================*/
+#define BUF_LEN          32
+#define CONF_LEN         10
 
-#include "nRF905_driver.h"
+#define NRF905_CSN       10
 
+unsigned char tx_buf[BUF_LEN]= "Hi, Arduino   \r\n";
+unsigned char read_config_buf[CONF_LEN];
 
-/*============================================================================*/
-/*                   LOCAL EXPORTED FUNCTION DECLARATIONS                     */
-/*============================================================================*/
+byte tx_address[4]= {0xcc,0xcc,0xcc,0xcc};
 
-
-/*============================================================================*/
-/*                           PRIVATE DIFINES                                  */
-/*============================================================================*/
-
-
-/*============================================================================*/
-/*                           LOCAL VARIABLES                                  */
-/*============================================================================*/
-
-
-/*============================================================================*/
-/*                           Hardware configuration                           */
-/*============================================================================*/
-
-
-/*==============================================================================
-** Function...: setup
-** Return.....: void
-** Description: Initial setup
-** Created....: 05.04.2015 by Achuthan
-** Modified...: dd.mm.yyyy by nn
-==============================================================================*/
 void setup()
 {
-  
+    unsigned char i;
+    
+    pinMode(NRF905_CSN,OUTPUT); //to make sure SPI works
+    
+    nrf905=NRF905(NRF905_CSN);
+    
+    nrf905.init();
+
+
+    /**
+        default configuration, need to specify frequency
+        choose Z-Wave frequency band, support :
+        US		908.42Mhz
+        EUROPE		868.42MHz
+        AFRICA		868.42MHz
+        CHINA		868.42MHz
+        HK		919.82MHz
+        JAPAN		853.42MHz
+        AUSTRALIA	921.42MHz
+        NEW_ZEALAND	921.42MHz
+        BRASIL		921.42MHz
+        RUSSIA		896MHz
+    */
+    nrf905.write_config(US);
+    nrf905.read_config(read_config_buf);
+
+    Serial.begin(9600);
+
+    for(i=0; i<10; i++)
+    {
+        Serial.print(read_config_buf[i],HEX);
+        Serial.print(' ');
+    }
+    
+    tx_buf[12] = '0';
 }
 
-
-/*==============================================================================
-** Function...: loop
-** Return.....: void
-** Description: main function
-** Created....: 05.04.2015 by Achuthan
-** Modified...: dd.mm.yyyy by nn
-==============================================================================*/
 void loop()
 {
-  
+    /** transmit data packet with default TX Address */
+    nrf905.TX(tx_buf);
+    
+    /** transmit data packet with specified TX Address */
+//    nrf905.TX(tx_buf, tx_address);
+    
+    // NOTE: TX_Address and RX_Address must be the same
+    
+    /** Count Sending times */
+    tx_buf[12]++;
+    if(tx_buf[12] == 0x3A){
+      tx_buf[12] = '0';
+    }
+    delay(50);
 }
+
